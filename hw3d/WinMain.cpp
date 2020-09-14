3acc7511b52a2f0f455501667afaeeb75fdae2b1
John Cook <coder-a599-7927@johncook.co.uk>
2020-09-14 12:16:45 +0100

    Code Analysis C28251: Inconsistent annotation for WinMain

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,10 +1,10 @@
 #include <Windows.h>
 
 int CALLBACK WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow)
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nCmdShow)
 {
 	return 0;
 }
